f6eadbe5db10cedbd4d5ce831b09a1498c11b953
Travis Griffiths <travis@travisgriffiths.com>
2025-12-11 14:30:36 -0300

    Basic CLI interface working

--- cli.py
+++ cli.py
@@ -6,3 +6,44 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
+nouns = {
+    'cnpj',
+    'poa',
+    'dof',
+    'transport'
+}
+
+verbs = {
+    'download',
+    'ingest'
+}
+
+def valid_pair(n: str, v: str):
+    if not n in nouns:
+        return False 
+    if not v in verbs:
+        return False
+    return True
+
+def prompt():
+    print('Action?')
+    cmd = input()
+    return verify_command(cmd)
+    
+
+def verify_command(cmd: str):
+    noun, verb = cmd.split()
+    if not valid_pair(noun, verb):
+        if not noun in nouns: 
+            print(f'{noun} not a recognized command, try one of: {', '.join([n for n in nouns])}')
+        elif not verb in verbs:
+            print(f'{verb} not a recognized action, try one of: {', '.join([v for v in verbs])}')
+        return prompt()
+    else:       
+        return [noun, verb]
+
+def cli(args: list[str] | None):
+    if args:
+       return verify_command(' '.join(args))
+    else:
+        return prompt()
--- main.py
+++ main.py
@@ -3,14 +3,42 @@
 from scripts.download.pmfs import downloadPMFS, unzipPMFS
 from scripts.download.cnpj import downloadCNPJ
 from scripts.config import PMFS_DIR, CNPJ_DIR
+from cli import cli
 import os
+import sys
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '.'))
 PMFS_ROUTE = f"{ROOT_DIR}/data-files/{PMFS_DIR}"
 CNPJ_ROUTE = f"{ROOT_DIR}/data-files/{CNPJ_DIR}"
 
+def cnpj(action: str):
+    if action == 'download':
+        print(f'This action can take several *HOURS* all files are multiple Gb in size. Continue? Y/N')
+        response = input()
+        if response.startswith(('Y', 'y')):
+            downloadCNPJ(CNPJ_ROUTE)
+        else:
+            print('Cancelling')
+    if action == 'ingest':
+        print('TODO: Build CNPJ ingest')
+
+def transport(action: str):
+    if action == 'download':
+        print('TODO: build download transport')
+    if action == 'ingest':
+        print('TODO: build ingest transport')
+
 def main():
-    downloadCNPJ(CNPJ_ROUTE)
+    noun, verb = None, None
+    if len(sys.argv) > 1:
+        noun, verb = cli(sys.argv[1:])
+    else:
+        noun, verb = cli(None)
+    if noun == 'cnpj':
+        cnpj(verb)
+    if noun == 'transport':
+        transport(verb)
+    #downloadCNPJ(CNPJ_ROUTE)
 
     # downloadPMFS(PMFS_ROUTE)
     # unzipPMFS(PMFS_ROUTE)
